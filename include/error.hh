bbd72aba7bf5d350b5d4d76063f7e01d4aee3c30
Salih MSA <salih.msa@outlook.com>
2023-04-01 02:30:33 +0100

    Need to make this more efficient and provide more details errors

--- include/error.hh
+++ include/error.hh
@@ -2,26 +2,41 @@
 #define ERROR_HH
 #pragma once
 
-#include <errno.h>
+#include <cerrno>
 
 #include <stdexcept>
 
 /**
- * @brief error.cc - file serves as CXX-style, exception-driven wrapper of POSIX C's error-code reporting mechanism
+ * @brief error.hh - file serves as CXX-style, exception-driven wrapper of POSIX C's error-code reporting mechanism
  * This file contains the declarations of such
  */
 
 namespace posicxx {
 
+#define POSICXX_UNDEFINED_ERROR -1
+
+	/**
+	 * @brief Error (class) - class thrown to manifest POSIX system errnos using CXX exceptions
+	 */
 	class Error : public std::runtime_error {
+		private:
+			int _errno ; // error number being thrown
+
 		public:
 			/**
-			 * @brief Error (constructor) - initialises values of exception
+			 * @brief Error (constructor) - initialises errno value and exception message
 			 *
 			 * @param const int errno - valid errno, as possibly returned by syscalls
 			 * See https://pubs.opengroup.org/onlinepubs/009695399/basedefs/errno.h.html for more details
+			 *
 			 */
 			Error(const int errno_n) noexcept ;
+
+			/**
+			 * @brief operator() - returns errno which error is made for
+			 * @return int - the internal errno
+			 */
+			int operator()() const noexcept ;
 
 			/* Below are the defaulted and deleted methods */
 			Error() noexcept = delete ;
@@ -34,4 +49,4 @@ namespace posicxx {
 
 }
 
-#endif // ERROR_HH
+#endif // #ifndef ERROR_HH
--- src/error.cc
+++ src/error.cc
@@ -1,5 +1,6 @@
-#include <string.h>
-#include <errno.h> // allows access to errno variable
+#include <cerrno> // allows access to errno variable
+#include <cstring>
+#include <string>
 
 #include "error.hh"
 
@@ -10,9 +11,31 @@
  */
 
 posicxx::Error::Error(const int errno_n) noexcept :
-	std::runtime_error(
-		static_cast<const char*>(
-			strerror(errno_n)
-		)
-	)
+	std::runtime_error( // give runtime_error an error message
+		[&]() -> std::string
+		{
+			/* if any of these cause an error, let it crash the system */
+			std::string error_str ;
+
+			if(errno_n != POSICXX_UNDEFINED_ERROR)
+			{
+				error_str = std::to_string(errno_n) ;
+				error_str += std::string(": ") ;
+				error_str += std::strerror(errno_n) ;
+			}
+			else {
+				error_str = std::string("Undefined error") ;
+			}
+
+			error_str += std::string(". Please refer to the appropriate manual / documentation") ;
+
+			return error_str ;
+		}() // remember to actually trigger a call to the function
+	),
+	_errno(errno_n) // keep the errno for future reference
 {}
+
+int posicxx::Error::operator()() const noexcept
+{
+	return this->_errno ;
+}
